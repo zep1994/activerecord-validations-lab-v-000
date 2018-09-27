e2f5be5c8a1f80cf58f720b0248dae7366fd4c99
Thomas Wesley Matlock <snaxbags@protonmail.com>
2018-09-27 20:04:54 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,18 +1,20 @@
 class Post < ActiveRecord::Base
-  validates :title, presence: true
-  validates :summary, length: {maximum: 250 }
-  validates :content, length: { minimum: 250 }
-  validates :category, inclusion: { in: %w(Fiction NonFiction) }
-  validate :is_clickbait?
-   CLICKBAIT_ARRAY = [
-    /Won't Believe/i,
-    /Secret/i,
-    /Top [0-9]*/i,
-    /Guess/i
-  ]
-   def is_clickbait?
-    if CLICKBAIT_ARRAY.none? {|clickbait| clickbait.match title}
-      errors.add(:title, "Must be clickbait-y")
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summaty, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :is_clickbait?
+
+    CLICKBAIT_PATTERNS = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top [0-9]*/i,
+        /Guess/i
+        ]
+
+    def is_clickbait?
+       if CLICKBAIT_PATTERNS.none? { |clickbait| clickbait.match title }
+         errors.add(:title, "must be clickbait-y")
+       end
     end
-  end
 end
