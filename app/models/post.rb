d02a5e742cf570257131a5a8b0b0588c61e94ef7
Thomas Wesley Matlock <snaxbags@protonmail.com>
2018-09-27 20:07:25 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
     validates :title, presence: true
-    validates :summary, length: { minimum: 250 }
-    validates :content, length: { maximum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :content, length: { minimum: 250 }
     validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
     validate :is_clickbait?
 
